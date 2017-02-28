1a47cd443ea27bf6b5604bb6061dd269e9456a81
Juan Vidal <juanvidalallende@gmail.com>
2017-02-28 14:50:10 +0100

    Check if password file exists before accessing it
    If the password fail does not exist, the script used to print a
    file-not-found error, which also made execution slower.

--- haste.bash
+++ haste.bash
@@ -2,7 +2,10 @@
 #place this file in your path. then send stdoud to | haste
 
 #optional for ldap or other simple auth in the format user.name:password@ (you need the @ after)
-password="$(< ~/.hidden_password)"
+if [ -f ~/.hidden_password ]
+then
+    password="$(< ~/.hidden_password)"
+fi
 
 #change this to your haste server
 url="hastebin.com"
